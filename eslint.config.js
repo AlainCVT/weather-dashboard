493d1a7cc18db8716da2baa1a95cbaff8a80af4e
Alain Cao Van Truong <cvt.alain@gmail.com>
2026-05-27 19:13:57 +0200

    chore(lint): improve linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,6 @@
 import js from '@eslint/js'
+import tsParser from '@typescript-eslint/parser'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
@@ -6,8 +8,9 @@ import globals from 'globals'
 export default [
   js.configs.recommended,
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
+      parser: tsParser,
       ecmaVersion: 'latest',
       sourceType: 'module',
       globals: globals.browser,
@@ -20,9 +23,13 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': 'off',
+      'react/self-closing-comp': 'error',
+      '@typescript-eslint/no-unused-vars': ['error'],
     },
   },
 ]
