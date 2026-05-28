15b6c11156ccda4c3ad536acb6cd903f982df50d
Alain Cao Van Truong <cvt.alain@gmail.com>
2026-05-28 22:53:09 +0200

    feat(map): use dark layer

--- src/api.ts
+++ src/api.ts
@@ -2,16 +2,16 @@ import { GeocodeResponseSchema, type GeocodeResponse } from '@/schemas/geocode'
 import type { Coords } from '@/types'
 import { WeatherResponseSchema, type WeatherResponse } from './schemas/weather'
 
-const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY
-const API_URL = import.meta.env.VITE_OPENWEATHER_API_URL
+const OPENWEATHER_API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY
+const OPENWEATHER_API_URL = import.meta.env.VITE_OPENWEATHER_API_URL
 
 export async function getWeather(coords: Coords): Promise<WeatherResponse> {
-  const url = new URL('data/3.0/onecall', API_URL)
+  const url = new URL('data/3.0/onecall', OPENWEATHER_API_URL)
 
   url.searchParams.append('lat', coords.lat.toString())
   url.searchParams.append('lon', coords.lon.toString())
   url.searchParams.append('units', 'metric')
-  url.searchParams.append('appid', API_KEY)
+  url.searchParams.append('appid', OPENWEATHER_API_KEY)
 
   const data = await fetch(url.toString()).then((result) => result.json())
 
@@ -21,11 +21,11 @@ export async function getWeather(coords: Coords): Promise<WeatherResponse> {
 export async function getGeocode(query: string): Promise<GeocodeResponse> {
   if (!query) return []
 
-  const url = new URL('geo/1.0/direct', API_URL)
+  const url = new URL('geo/1.0/direct', OPENWEATHER_API_URL)
 
   url.searchParams.append('q', query)
   url.searchParams.append('limit', '1')
-  url.searchParams.append('appid', API_KEY)
+  url.searchParams.append('appid', OPENWEATHER_API_KEY)
 
   const data = await fetch(url.toString()).then((result) => result.json())
 
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,13 @@
 import type { Coords } from '@/types'
+import { MaptilerLayer } from '@maptiler/leaflet-maptilersdk'
 import 'leaflet/dist/leaflet.css'
 import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet'
 
 import type { MapType } from '@/components/dropdowns/MapTypeDropdown'
+import { useEffect } from 'react'
 
-const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY
+const MAPTILER_API_KEY = import.meta.env.VITE_MAPTILER_API_KEY
+const OPENWEATHER_API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY
 
 type MapProps = {
   coords: Coords
@@ -28,6 +31,24 @@ const MapClick = ({ coords, onMapClick }: MapProps) => {
   return null
 }
 
+const MapTileLayer = () => {
+  const map = useMap()
+
+  useEffect(() => {
+    const tileLayer = new MaptilerLayer({
+      style: 'basic-dark',
+      apiKey: MAPTILER_API_KEY,
+    })
+    tileLayer.addTo(map)
+
+    return () => {
+      map.removeLayer(tileLayer)
+    }
+  }, [map])
+
+  return null
+}
+
 export default function Map({ coords, onMapClick, mapType }: Props) {
   const { lat, lon } = coords
 
@@ -38,9 +59,9 @@ export default function Map({ coords, onMapClick, mapType }: Props) {
       style={{ width: '100%', height: '500px' }}
     >
       <MapClick coords={coords} onMapClick={onMapClick} />
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <MapTileLayer />
       <TileLayer
-        url={`https://tile.openweathermap.org/map/${mapType}/{z}/{x}/{y}.png?appid=${API_KEY}`}
+        url={`https://tile.openweathermap.org/map/${mapType}/{z}/{x}/{y}.png?appid=${OPENWEATHER_API_KEY}`}
       />
       <Marker position={[lat, lon]} />
     </MapContainer>
