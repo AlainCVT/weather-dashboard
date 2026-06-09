07a0709424900003a6412a042895a192658c0cfb
Alain Cao Van Truong <cvt.alain@gmail.com>
2026-06-09 18:41:21 +0200

    fix(ui): put panel button to sticky position

--- src/components/partials/SidePanel.tsx
+++ src/components/partials/SidePanel.tsx
@@ -36,7 +36,7 @@ export default function SidePanel({ coords, className }: Props) {
     <aside
       className={clsx(
         className,
-        'bg-sidebar border-accent grid h-screen w-xs content-start gap-4 overflow-auto border-l px-4 py-6 shadow-md duration-400 not-lg:transition-transform',
+        'bg-sidebar border-accent grid h-screen w-xs content-start overflow-auto border-l shadow-md duration-400 not-lg:transition-transform',
         {
           'not-lg:-translate-x-full': isOpen,
           'not-lg:pointer-none': !isOpen,
@@ -44,23 +44,26 @@ export default function SidePanel({ coords, className }: Props) {
       )}
       {...(!isOpen && { tabIndex: -1 })}
     >
-      <Button
-        className="lg:hidden"
-        variant="secondary"
-        onClick={() => {
-          toggleState('close')
-        }}
-      >
-        <Icon
-          name="Chevron"
-          size={16}
-          className="text-muted-foreground -rotate-90"
-        />
-        Close
-      </Button>
-      <Suspense fallback={<AirPollutionStatsSkeleton />}>
-        <AirPollutionStats coords={coords} />
-      </Suspense>
+      <div className="before:bg-size-full before:from-sidebar before:to-sidebar/0 pointer-events-none sticky top-0 z-1 px-4 py-6 pb-4 *:pointer-events-auto *:relative before:pointer-events-none before:absolute before:inset-0 before:bg-linear-to-b lg:hidden">
+        <Button
+          variant="secondary"
+          onClick={() => {
+            toggleState('close')
+          }}
+        >
+          <Icon
+            name="Chevron"
+            size={16}
+            className="text-muted-foreground -rotate-90"
+          />
+          Close
+        </Button>
+      </div>
+      <div className="px-4 pb-6 lg:pt-6">
+        <Suspense fallback={<AirPollutionStatsSkeleton />}>
+          <AirPollutionStats coords={coords} />
+        </Suspense>
+      </div>
     </aside>
   )
 }
